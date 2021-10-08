72aed06f4b5761de0615b8e1a575176b2e53717f
Pervez Ali <contact.pervezali@gmail.com>
2021-10-08 22:36:26 +0600

    user authentiacation driver set to session

---
+++ database/migrations/1633710541485_users.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class UsersSchema extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('first_name', 191)
+      table.string('last_name', 191)
+      table.string('username', 191).unique()
+      table.string('email', 191).unique()
+      table.string('avatar', 191).defaultTo('default_avatar.png').nullable()
+      table.enum('gender', ['male', 'female', 'other'])
+      table.string('password', 191)
+      table.string('remember_me_token').nullable()
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
