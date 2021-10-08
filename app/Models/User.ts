8c582004a9d2e419eb362a547bf99504a6a4b69c
Pervez Ali <contact.pervezali@gmail.com>
2021-10-08 22:26:00 +0600

    user migration and model  setup complete

---
+++ app/Models/User.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public first_name: string
+
+  @column()
+  public last_name: string
+
+  @column()
+  public username: string
+
+  @column()
+  public email: string
+
+  @column()
+  public avatar: string
+
+  @column()
+  public gender: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
